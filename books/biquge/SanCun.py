0c6b51a5e53592bd804a29dacaf64c480f8dcca8
自定义acl <35255028+99junlin99@users.noreply.github.com>
2019-10-12 10:44:48 +0800

    Create SanCun.py

---
+++ books/biquge/SanCun.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import xxbiqugebase
+
+
+def getBook():
+    return SanCun
+
+
+class SanCun(xxbiqugebase):
+    title = u'[小说]三寸人间'
+    description = u'耳根'
+    feeds = 'https://www.xbiquge6.com/34_34822/'
